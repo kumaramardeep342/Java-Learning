949b2da405ce3fa592d5db558fe7cdd8f6d91716
kumaramardeep342 <kumaramardeep342@gmail.com>
2023-03-26 22:27:04 +0530

    First Commit

---
+++ app01/src/com/durgasoft/test/Test.java
@@ -0,0 +1,11 @@
+package com.durgasoft.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Durgasoft");
+
+	}
+
+}
